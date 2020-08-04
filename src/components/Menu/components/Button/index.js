798d0fedfd24a4e464d2352ab9b2712f1357d6f3
Vittoria Thomasini <vittoriathomasini@hotmail.com>
2020-08-04 08:30:48 -0300

    Correction

--- src/components/Menu/components/Button/index.js
+++ src/components/Menu/components/Button/index.js
@@ -1,11 +1,12 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable react/destructuring-assignment */
 import React from 'react';
 
-function Button(props){
-    console.log(props);
-    return(
-        <a href="/" className={props.className}>
-           {props.children}
-        </a>
-    )
+function Button(props) {
+  return (
+    <a href="/" className={props.className}>
+      {props.children}
+    </a>
+  );
 }
 export default Button;
--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,22 +1,22 @@
-
+/* eslint-disable jsx-quotes */
 import React from 'react';
-import Logo from '../../assets/img/Logo.jpg';
-import './Menu.css'
 import { Link } from 'react-router-dom';
-import Button from '../Button'
+import Logo from '../../assets/img/Logo.jpg';
+import './Menu.css';
+import Button from '../Button';
 
-function Menu(){
-    return (
-        <header>
-            <nav className="Menu">
-                <Link to='/'>
-                <img className="Logo" src= {Logo} alt="Cyber-logo" to ='/'/>
-                </Link>
-                <Button as={Link} className="ButtonLink" to ="/cadastro/video">
-                   Novo Vídeo
-                </Button>
-            </nav>
-        </header>
-    );
+function Menu() {
+  return (
+    <header>
+      <nav className="Menu">
+        <Link to='/'>
+          <img className="Logo" src={Logo} alt="Cyber-logo" to='/' />
+        </Link>
+        <Button as={Link} className="ButtonLink" to="/cadastro/video">
+          Novo Vídeo
+        </Button>
+      </nav>
+    </header>
+  );
 }
 export default Menu;
